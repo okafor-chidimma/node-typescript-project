c2ea40ceaf9d61bfc36174e5615d407247d6a34f
Okafor Chidimma <chidimma.okafor.c@gmail.com>
2021-06-12 17:53:39 +0100

    update to the folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 /*
     you can use babel to transpile ts and run it via babel node with nodemon but it will not check for types. to check for types, we can use tsc
+    when using babel, it does not spit out js files after compiling ts files, instead it compiles ts files and runs in the terminal or browser but no output file is dropped in the dist folder
+
+    when using babel and tsc, you have to configure tsconfig file to not spit files, rather it should just check for types error.
+    you can tell it to spit out js files, but it is not useful since babel still runs the ts files and not js
+
+    using babel and tsc, it is not ideal since you cannot use this combination in prod as you cannot run ts files in the browser or using node, so the best combo is tsc and nodemon
+
+
     
     on the other hand, we can use tsc to transpile ts to js and use nodemon to run the js output files
+    tsc compiles down all ts files, including the import statements and all the es6 syntax as specified in the tsconfig file and nodemon runs the compiled down js files
 
 */
 
